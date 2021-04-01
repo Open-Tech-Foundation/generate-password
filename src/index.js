551d2def26f8bb2060cdff2c403bff079fb41997
Thanga Ganapathy <ganapathy888@gmail.com>
2021-04-01 10:55:44 +0530

    Generate passwords using crypto random values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-function generatePassword() {
-  return 'Hello world!';
+const numbers = '0123456789';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const symbols = '!@#$%^&*';
+const characters = numbers + lowerCase + upperCase + symbols;
+
+function generate(rules) {
+  let password = '';
+  const array = new Uint8Array(1);
+  for (let i = 0; i < rules.length; i++) {
+    const c = crypto.getRandomValues(array)[0];
+    password += characters[c % characters.length];
+  }
+  return password;
+}
+
+function generatePassword(options) {
+  options = options || {};
+  const rules = {
+    length: options.length || 8,
+  };
+
+  return generate(rules);
 }
 
 export default generatePassword;
